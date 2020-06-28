e73aeacb2a0545138e974b84e846dc4fda52e82d
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-06-28 01:28:33 -0300

    Go: Isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,17 @@
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram is a function that checks if the passed word is an isogram
+func IsIsogram(word string) bool {
+	word = strings.ToLower(word)
+	for _, l := range word {
+		if strings.Count(word, string(l)) > 1 && unicode.IsLetter(l) {
+			return false
+		}
+	}
+	return true
+}
