7eb19ad592fec262cb95b8da0a1ed945c088b1a4
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-07-03 10:27:20 -0300

    a better solution

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,17 +1,10 @@
 object Acronym {
-    fun generate(phrase: String): String {
-        var acronym = phrase[0].toString().toUpperCase()
-        phrase
-            .replace("- ", "")
-            .replace("-", " ")
-            .replace("_", "")
-            .apply {
-                forEachIndexed { index, c ->
-                    if (c.isWhitespace())
-                        acronym += this[index + 1].toUpperCase()
-                }
-            }
 
-        return acronym
+    fun generate(phrase: String): String {
+        return phrase
+            .splitToSequence(" ", "_", "-")
+            .filter { it.isNotBlank() }
+            .map { it.first().toUpperCase() }
+            .joinToString("")
     }
 }
