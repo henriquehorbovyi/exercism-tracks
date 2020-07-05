e610b755b54b4f4a277cc6b72c6cf8d15041d931
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-07-05 16:12:44 -0300

    added some new problems

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -2,9 +2,10 @@ object Acronym {
 
     fun generate(phrase: String): String {
         return phrase
+            .toUpperCase()
             .splitToSequence(" ", "_", "-")
             .filter { it.isNotBlank() }
-            .map { it.first().toUpperCase() }
+            .map { it.first() }
             .joinToString("")
     }
 }
---
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -0,0 +1,14 @@
+class Anagram(private val source: String) {
+
+    private val isSourceAnagram: (String) -> Boolean = { word ->
+        source.count() == word.count() &&
+            !source.equals(word, ignoreCase = true) &&
+            word.matches("([$source])\\w+".toRegex(RegexOption.IGNORE_CASE))
+    }
+
+    fun match(anagrams: Collection<String>): Set<String> {
+        return anagrams
+            .filter { word -> isSourceAnagram(word) }
+            .toSet()
+    }
+}
