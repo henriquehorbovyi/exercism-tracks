7c3d87f2ac07562a6223a004513d80c26744d6ba
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-07-09 12:38:42 -0300

    Improving Hamming

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -2,10 +2,9 @@ import java.lang.IllegalArgumentException
 
 object Hamming{
     fun compute(dna1: String, dna2: String): Int {
-        return if(dna1.length == dna2.length){
-            (dna1 zip dna2).count{ (first, second) -> first != second }
-        }else{
+        if (dna1.length != dna2.length)
             throw IllegalArgumentException("left and right strands must be of equal length.")
-        }
+
+        return (dna1 zip dna2).count{ (first, second) -> first != second }
     }
 }
